02b9de36335d3073e6ebb54a8cd5f6093a61dff6
Kevin <kevin.deguzman@viewqwest.com>
2019-08-02 09:42:06 +0800

    Create route for fixed url

--- deskara/src/app/app.module.ts
+++ deskara/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; 
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, MainRoutes } from './app-routing.module';
 import { AppComponent } from './app.component'; 
 import { FeatureSectionComponent } from './feature-section/feature-section.component';
 import { TestimonialSectionComponent } from './testimonial-section/testimonial-section.component';
 import { FooterSectionComponent } from './footer-section/footer-section.component';
 import { SharedComponent } from './shared/shared.component';
 import { CountriesService } from "./shared/services/countries.service";
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { CountriesService } from "./shared/services/countries.service";
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(MainRoutes),
   ],
   providers: [
     CountriesService
